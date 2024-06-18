78bdc7532037a97ed8117e2b1b47ab4cf67e35a5
Apolos666 <quangprox121@gmail.com>
2024-06-18 22:15:02 +0700

    feat: Use @greatsumini/react-facebook-login package for Facebook authentication react side

--- frontend/src/components/ExternalLogin/FacebookButton.tsx
+++ frontend/src/components/ExternalLogin/FacebookButton.tsx
@@ -1,12 +1,31 @@
 import {FaFacebookF} from "react-icons/fa";
+import FacebookLogin from "@greatsumini/react-facebook-login";
 
 function FacebookButton({ className } : {className?: string}) {
     return (
         <>
-            <button className={`${className} flex items-center gap-4 py-2 px-6 bg-[#3B5998]`}>
-                <FaFacebookF className="text-white"/>
-                <div className="text-white">Facebook</div>
-            </button>
+            {/* Only work for development with same facebook account */}
+            <FacebookLogin
+                appId="1879188532554962"
+                onSuccess={(response) => {
+                    console.log('Login Success!', response);
+                }}
+                onFail={(error) => {
+                    console.log('Login Failed!', error);
+                }}
+                onProfileSuccess={(response) => {
+                    console.log('Get Profile Success!', response);
+                }}
+                render={({ onClick }) => (
+                    <button onClick={onClick} className={`${className} flex items-center gap-4 py-2 px-6 bg-[#3B5998]`}>
+                        <FaFacebookF className="text-white"/>
+                        <div className="text-white">Facebook</div>
+                    </button>
+                )}
+                loginOptions={{
+                    auth_type: "reauthenticate"
+                }}
+            />
         </>
     )
 }
