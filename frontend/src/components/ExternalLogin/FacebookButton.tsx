0dfe98bc28c64cd9654e5ee42077e4946e932d98
Apolos666 <quangprox121@gmail.com>
2024-06-18 22:32:54 +0700

    security: remove appid from component and remove app on meta for developer, saved in .env.development.local file

--- frontend/src/components/ExternalLogin/FacebookButton.tsx
+++ frontend/src/components/ExternalLogin/FacebookButton.tsx
@@ -1,21 +1,23 @@
 import {FaFacebookF} from "react-icons/fa";
-import FacebookLogin from "@greatsumini/react-facebook-login";
+import FacebookLogin, {ProfileSuccessResponse} from "@greatsumini/react-facebook-login";
 
 function FacebookButton({ className } : {className?: string}) {
+    function handleProfileSuccess(response: ProfileSuccessResponse) {
+        console.log('Get Profile Success!', response);
+    }
+
     return (
         <>
             {/* Only work for development with same facebook account */}
             <FacebookLogin
-                appId="1879188532554962"
+                appId={import.meta.env.VITE_FACEBOOK_APP_ID} // Đã xoá App cũ trên meta for developer và tạo lại cái mới
                 onSuccess={(response) => {
                     console.log('Login Success!', response);
                 }}
                 onFail={(error) => {
                     console.log('Login Failed!', error);
                 }}
-                onProfileSuccess={(response) => {
-                    console.log('Get Profile Success!', response);
-                }}
+                onProfileSuccess={handleProfileSuccess}
                 render={({ onClick }) => (
                     <button onClick={onClick} className={`${className} flex items-center gap-4 py-2 px-6 bg-[#3B5998]`}>
                         <FaFacebookF className="text-white"/>
