f57756deef2e8afa14128417c85a9661f39640fb
Apolos666 <quangprox121@gmail.com>
2024-06-19 22:24:43 +0700

    feat: Add lazy loading for search header placeholder
    This commit adds lazy loading functionality to the search header placeholder in the `SearchHeader` component. The placeholder text is now generated using the `useTypingEffect` hook from the `useTypingEffect.tsx` file. This improves the user experience by providing a more engaging and dynamic placeholder text for the search input field.

--- frontend/src/components/SearchHeader/SearchHeader.tsx
+++ frontend/src/components/SearchHeader/SearchHeader.tsx
@@ -1,7 +1,11 @@
 import {Input} from "@/components/ui/input.tsx";
 import {IoMdSearch} from "react-icons/io";
+import { searchDataHeaderPlaceHolder } from "./SearchHeaderConfig.ts"
+import {useTypingEffect} from "@/hooks/useTypingEffect.tsx";
 
 export function SearchHeader({widthPercent}: {widthPercent: string}) {
+    const placeholder = useTypingEffect(searchDataHeaderPlaceHolder);
+
     function submitSearch() {
 
     }
@@ -11,7 +15,7 @@ export function SearchHeader({widthPercent}: {widthPercent: string}) {
             <form className={`relative ${widthPercent}`}>
                 <Input className="bg-white rounded-[10px] text-gray-500 border-white py-6 pr-10"
                        type="text"
-                       placeholder="Tạm"
+                       placeholder={placeholder}
                 />
                 <IoMdSearch onClick={submitSearch}
                             className="absolute transforn -translate-y-1/2 top-1/2 right-2 text-2xl text-[#1B74E7] cursor-pointer"
