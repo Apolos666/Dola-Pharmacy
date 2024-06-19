5658f9ac091ffa67b1afc4f2de33c46016d5054c
Apolos666 <quangprox121@gmail.com>
2024-06-19 13:04:46 +0700

    chore: Don't allow breadcrumb from appearing on the home page

--- frontend/src/components/BreadCrumb/BreadCrumbCustom.tsx
+++ frontend/src/components/BreadCrumb/BreadCrumbCustom.tsx
@@ -1,9 +1,10 @@
-import {useMatches} from "react-router-dom";
+import {useLocation, useMatches} from "react-router-dom";
 import {
     Breadcrumb,
     BreadcrumbItem,
     BreadcrumbLink,
-    BreadcrumbList, BreadcrumbPage,
+    BreadcrumbList,
+    BreadcrumbPage,
     BreadcrumbSeparator
 } from "@/components/ui/breadcrumb.tsx";
 import {Fragment} from "react";
@@ -11,33 +12,40 @@ import "@/app/app.css"
 
 export function BreadCrumbCustom() {
     const matches = useMatches();
+    const location = useLocation();
 
+    if (location.pathname === "/") // If the current path is the home page then don't show the crumb
+        return null;
+
+    // todo: nếu ở trang home thì sẽ không lấy cái crumb của nó để hiện thị
     let crumbs = matches
         .filter((match) => Boolean(match.handle?.crumb))
         .map((match) => match.handle!.crumb());
 
     return (
         <>
-            <Breadcrumb className="container-app">
-                <BreadcrumbList>
-                    {crumbs.map((crumb, index) => (
-                        <Fragment key={index}>
-                            <BreadcrumbItem>
-                                {index < crumbs.length - 1 ? ( // If not the last crumb then add a link
-                                    <BreadcrumbLink className="text-base" href={matches[index].pathname}>
-                                        {crumb}
-                                    </BreadcrumbLink>
-                                ) : (
-                                    <BreadcrumbPage className="text-base text-[#003cbf]">
-                                        {crumb}
-                                    </BreadcrumbPage>
-                                )}
-                            </BreadcrumbItem>
-                            {index < crumbs.length - 1 && <BreadcrumbSeparator/>} {/* Add separator if not the last crumb */}
-                        </Fragment>
-                    ))}
-                </BreadcrumbList>
-            </Breadcrumb>
+            <div className="h-14 bg-[#D9E6FF] text-center">
+                <Breadcrumb className="container-app">
+                    <BreadcrumbList>
+                        {crumbs.map((crumb, index) => (
+                            <Fragment key={index}>
+                                <BreadcrumbItem>
+                                    {index < crumbs.length - 1 ? ( // If not the last crumb then add a link
+                                        <BreadcrumbLink className="text-base" href={matches[index].pathname}>
+                                            {crumb}
+                                        </BreadcrumbLink>
+                                    ) : (
+                                        <BreadcrumbPage className="text-base text-[#003cbf]">
+                                            {crumb}
+                                        </BreadcrumbPage>
+                                    )}
+                                </BreadcrumbItem>
+                                {index < crumbs.length - 1 && <BreadcrumbSeparator/>} {/* Add separator if not the last crumb */}
+                            </Fragment>
+                        ))}
+                    </BreadcrumbList>
+                </Breadcrumb>
+            </div>
         </>
     )
 }
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,9 +6,7 @@ function Header() {
         <>
             <div className="h-20 bg-[#81E0E2] text-center">Header Banner</div>
             <div className="h-44 bg-[#2772FA] text-center">Header</div>
-            <div className="h-14 bg-[#D9E6FF] text-center">
-                <BreadCrumbCustom />
-            </div>
+            <BreadCrumbCustom />
         </>
     )
 }
