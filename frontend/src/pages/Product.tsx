c51619001d3b555f0c59a958ea812b89d43c8787
Apolos666 <quangprox121@gmail.com>
2024-07-04 16:04:16 +0700

    feat: Complete the ProductTypeRelation to provide the ability to conveniently navigate between product types.

--- frontend/src/pages/Product.tsx
+++ frontend/src/pages/Product.tsx
@@ -1,9 +1,10 @@
 import {ProductSection} from "@/components/ProductSection/ProductSection.tsx";
+import {ProductTypeRelation} from "@/components/ProductTypeRelation/ProductTypeRelation.tsx";
 
 export function Product() {
     return (
         <div className="container-app">
-            <div className="h-[157px] bg-orange-300">Place Holder</div>
+            <ProductTypeRelation />
             <ProductSection />
         </div>
     )
